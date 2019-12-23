29a59d5dc4e8b302f9b7ffdff56b638b1e04fdfa
mauriciosilv <mausilva828@gmail.com>
2019-12-23 16:49:38 -0300

    date and time

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,31 +1,60 @@
 class CalcController {
 
   constructor() {
-    this._displayCalc = "0";
+    this._locale = 'pt-BR';
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
     this._currentDate;
     this.initalize();
   }
 
   initalize() {
-    let displayCalcEl = document.querySelector("#display");
-    let dateEl = document.querySelector("#data");
-    let timeEl = document.querySelector("#hora");
 
-    displayCalcEl.innerHTML = "2323";
-    dateEl.innerHTML = "23/12/2019";
-    timeEl.innerHTML = "16:19";
+    this.setDisplayDateTime();
+
+    setInterval(() => {
+      this.setDisplayDateTime();
+
+    }, 1000);
+
+
+  }
+
+  setDisplayDateTime() {
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+      day: "2-digit",
+      month: "long",
+      year: "numeric"
+    });
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+  }
+
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
+
+  set displayTime(value) {
+    this._timeEl.innerHTML = value;
+  }
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
+
+  set displayDate(value) {
+    this._dateEl.innerHTML = value;
   }
 
   get displayCalc() {
-    return this._displayCalc;
+    return this._displayCalcEl.innerHTML;
   }
 
-  set displayCalc(valor) {
-    this._displayCalc = valor;
+  set displayCalc(value) {
+    this._displayCalcEl.innerHTML = value;
   }
 
   get currentDate() {
-    return this._currentDate;
+    return new Date();
   }
 
   set currentDate(date) {
