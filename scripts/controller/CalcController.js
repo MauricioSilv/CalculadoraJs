0d6231389e2c1a5c11f390014b82b83bc4404ce2
mauriciosilv <mausilva828@gmail.com>
2019-12-23 16:21:12 -0300

    DOM manipulation

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,24 +1,35 @@
-class CalcController{
+class CalcController {
 
-  constructor(){
+  constructor() {
     this._displayCalc = "0";
-    this._dataAtual;
+    this._currentDate;
+    this.initalize();
   }
 
-  get displayCalc(){
+  initalize() {
+    let displayCalcEl = document.querySelector("#display");
+    let dateEl = document.querySelector("#data");
+    let timeEl = document.querySelector("#hora");
+
+    displayCalcEl.innerHTML = "2323";
+    dateEl.innerHTML = "23/12/2019";
+    timeEl.innerHTML = "16:19";
+  }
+
+  get displayCalc() {
     return this._displayCalc;
   }
 
-  set displayCalc(valor){
+  set displayCalc(valor) {
     this._displayCalc = valor;
   }
 
-  get dataAtual(){
-    return this._dataAtual;
+  get currentDate() {
+    return this._currentDate;
   }
 
-  set dataAtual(data){
-    this._dataAtual = data;
+  set currentDate(date) {
+    this._currentDate = date;
   }
 
 
